22d0257d0adf0eb74cac7679202ae90087cea35f
Felix Lam <flam.905@gmail.com>
2019-01-10 18:12:56 -0500

    add css

--- src/mds/web/static/web/js/chosen-multiselect.js
+++ src/mds/web/static/web/js/chosen-multiselect.js
@@ -1,6 +1,12 @@
 // Assumes chosen and jQuery have already been imported
 
 $(document).ready(function() {
-    $("select[multiple]").chosen();
-    $(".chosen-container").css({"min-width": 135});
+    $("select[multiple]").each(function(index,el) {
+      var $el = $(el);
+      if ($el.children().length === 0) {
+          $el.chosen({disable_search: true});
+      } else {
+          $el.chosen();
+      }
+    });
 });
