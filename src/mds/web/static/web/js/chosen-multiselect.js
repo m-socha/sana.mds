c62105ad2edc561d1dedaf10fe1c4d22346bacbc
Felix Lam <flam.905@gmail.com>
2019-01-10 18:22:44 -0500

    add css

--- src/mds/web/static/web/js/chosen-multiselect.js
+++ src/mds/web/static/web/js/chosen-multiselect.js
@@ -3,9 +3,7 @@
 $(document).ready(function() {
     $("select[multiple]").each(function(index,el) {
       var $el = $(el);
-      if ($el.children().length === 0) {
-          $el.parent().text("None available")
-      } else {
+      if ($el.children().length !== 0) {
           $el.chosen();
       }
     });
