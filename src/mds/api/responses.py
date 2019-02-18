025c6508d0a1cd264a2e6527079f31b54116d26f
Felix Lam <flam.905@gmail.com>
2019-02-18 08:51:03 -0500

    Serialization

--- src/mds/api/responses.py
+++ src/mds/api/responses.py
@@ -72,7 +72,7 @@ def fail(data, code=404, errors=[]):
 
 def serializeModels(models):
     data = serializers.serialize('json', models)
-    data = json.load(data)
+    data = json.loads(data)
     to_return = []
     for el in data:
         to_return.append(el['fields'])
